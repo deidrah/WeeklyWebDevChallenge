0a287a4ad794cfa84892d386f938cd3c834b751e
deidrah <kumkwaczka@gmail.com>
2018-05-03 23:19:01 +0200

    adding first part of the header

--- 24/gulpfile.js
+++ 24/gulpfile.js
@@ -1,17 +1,53 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
 const autoprefixer = require('gulp-autoprefixer');
+const pump = require('pump');
+const concat = require('gulp-concat');
+const browserSync = require('browser-sync').create();
 
-gulp.task('sass', function(){
-  return gulp.src('scss/styles.scss')
-    .pipe(sass())
-    .pipe(autoprefixer({
-        browsers: ['last 3 versions'],
-        cascade: false
-        }))
-    .pipe(gulp.dest('css'))
+gulp.task('js', (callback) => {
+  pump(
+    [
+      gulp.src([
+        'node_modules/jquery/dist/jquery.min.js',
+        'node_modules/bootstrap/dist/js/bootstrap.min.js',
+        'node_modules/owl.carousel/dist/owl.carousel.min.js',
+        'js/scripts.js'
+      ]),
+      concat('scripts.js'),
+      gulp.dest('public/assets'),
+      browserSync.stream(),
+    ],
+    callback
+  );
 });
 
-gulp.task('default', function() {
-    gulp.watch(['scss/*.scss'], ['sass'])
+gulp.task('sass', (callback) => {
+  pump(
+    [
+      gulp.src('scss/styles.scss'),
+      sass(),
+      autoprefixer({
+        browsers: ['last 3 versions'],
+        cascade: false,
+      }),
+      gulp.dest('public/assets'),
+      browserSync.stream(),
+    ],
+    callback
+  );
+});
+
+gulp.task('default', () => {
+  browserSync.init({
+    server: './public/',
+    host: '0.0.0.0',
+  });
+
+  gulp.watch(['scss/*.scss'], ['sass']);
+  gulp.watch(['js/*.js'], ['js']);
+
+  gulp
+    .watch('public/*.html')
+    .on('change', browserSync.reload);
 });
---
+++ 24/js/scripts.js
@@ -0,0 +1,7 @@
+$(document).ready(function () {
+    $(".owl-carousel").owlCarousel({
+		items: 1,
+        autoplay: false,
+        loop: false,
+	});
+});
